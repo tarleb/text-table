0e48190a3a80383ebd4ccd6e93d9d5d47099c436
Albert Krewinkel <albert@zeitkraut.de>
2022-08-31 12:57:59 +0200

    Add module for Cell type

---
+++ src/Text/Table/Cell.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies               #-}
+{- |
+Module      : Text.Table.Cell
+Copyright   : © 2022 RStudio, PBC
+License     : MIT
+Maintainer  : Albert Krewinkel <albert@zeitkraut.de>
+
+Typeclass for textual tables.
+-}
+
+module Text.Table.Cell
+  ( Cell (..)
+  , RowSpan (..)
+  , ColSpan (..)
+  ) where
+
+-- | A table cell, the smallest unit of a table.
+data Cell a = Cell
+  { cellContent :: a
+  , cellRowSpan :: RowSpan
+  , cellColSpan :: ColSpan
+  }
+  deriving stock (Eq, Ord, Show)
+
+-- | The number of rows spanned by a cell.
+newtype RowSpan = RowSpan Int
+  deriving stock (Eq, Ord)
+  deriving newtype (Enum, Num, Read, Show)
+
+-- | The number of columns spanned by a cell.
+newtype ColSpan = ColSpan Int
+  deriving stock (Eq, Ord)
+  deriving newtype (Enum, Num, Read, Show)
