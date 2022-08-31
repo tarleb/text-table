0fcd0d873b2a94ab3acc0491a28d135459419427
Albert Krewinkel <albert@zeitkraut.de>
2022-08-31 12:58:37 +0200

    Add draft for a Table typeclass

---
+++ src/Text/Table/Class.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE TypeFamilies               #-}
+{- |
+Module      : Text.Table.Class
+Copyright   : © 2022 RStudio, PBC
+License     : MIT
+Maintainer  : Albert Krewinkel <albert@zeitkraut.de>
+
+Typeclass for textual tables.
+-}
+
+module Text.Table.Class
+  ( Table (..)
+  , Cell (..)
+  , RowSpan (..)
+  , ColSpan (..)
+  ) where
+
+import Text.Table.Cell
+
+class Table tbl where
+  data Content tbl :: *
+  data Row tbl :: *
+  allCells     :: tbl -> [Cell (Content tbl)]
+  allRows      :: tbl -> [Row tbl]
+  headRows     :: tbl -> [Row tbl]
